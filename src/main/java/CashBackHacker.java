403d54a7fadccb7d74e72d7f777cbcfe0692e883
Koloshko Evgeni <kolohkoe@gmail.com>
2023-04-16 17:09:00 +0700

    Code testing

--- src/main/java/CashBackHacker.java
+++ src/main/java/CashBackHacker.java
@@ -1,2 +1,6 @@
 public class CashBackHacker {
+    private final int boundary = 1000;
+    public int remain(int amount) {
+        return boundary - amount % boundary;
+    }
 }
--- src/test/java/CashBackHackerTest.java
+++ src/test/java/CashBackHackerTest.java
@@ -1,4 +1,34 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class CashBackHackerTest {
-  
+
+    @Test
+    void calculateCashBack (){
+        CashBackHacker servis = new CashBackHacker();
+        int actual = servis.remain(900);
+        int expected = 100;
+        assertEquals(expected,actual);
+
+    }
+
+    @Test
+    void calculateCashBack2 (){
+        CashBackHacker servis = new CashBackHacker();
+        int actual = servis.remain(1500);
+        int expected = 500;
+        assertEquals(expected,actual);
+
+    }
+
+    @Test
+    void calculateCashBack3 (){
+        CashBackHacker servis = new CashBackHacker();
+        int actual = servis.remain(2000);
+        int expected = 0;
+        assertEquals(expected,actual);
+
+    }
+
 }
