a25dc2aa3fb32c664f1c8f1f96fb4c2fa019ec4f
Koloshko Evgeni <kolohkoe@gmail.com>
2023-04-16 18:37:19 +0700

    Fixed Branch testNG

--- src/test/java/CashBackHackerTest.java
+++ src/test/java/CashBackHackerTest.java
@@ -1,7 +1,6 @@
-import org.junit.jupiter.api.Test;
+import org.testng.annotations.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.testng.Assert.assertEquals;
+import static org.testng.AssertJUnit.assertEquals;
 
 class CashBackHackerTest {
 
